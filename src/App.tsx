2d56657a4483f4f6dfdd07c70687b2e6124d2ab6
Justin <xhergz@gmail.com>
2021-07-11 15:06:00 -0400

    Build trigger.

--- src/App.tsx
+++ src/App.tsx
@@ -49,7 +49,7 @@ function App() {
                     </Switch>
                 </main>
                 <footer className='page-footer'>
-                    Version 0.1.1
+                    Version 0.1.2
                 </footer>
             </div>
         </Router>
