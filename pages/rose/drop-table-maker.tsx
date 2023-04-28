d548f635fbf24256764a84c9ec75d2edd12b04ab
Justin <xhergz@gmail.com>
2023-04-28 10:47:35 -0400

    Added page placeholder for drop table maker

---
+++ pages/rose/drop-table-maker.tsx
@@ -0,0 +1,13 @@
+import { NextPage } from "next";
+
+import PageWrapper from "../../src/components/PageWrapper/PageWrapper";
+
+const DropTableMaker: NextPage = () => {
+  return (
+    <PageWrapper>
+      <div>Hello World</div>
+    </PageWrapper>
+  );
+};
+
+export default DropTableMaker;
