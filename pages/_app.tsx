ef375b7947360d0540e88f327d2352cba0156caa
Justin <xhergz@gmail.com>
2023-04-28 10:17:40 -0400

    Removed mantine dependency.
    We will be moving to tailwind and headless ui for everything.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,8 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import { MantineProvider } from "@mantine/core";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <MantineProvider withGlobalStyles withNormalizeCSS>
-      <Component {...pageProps} />
-    </MantineProvider>
-  );
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
