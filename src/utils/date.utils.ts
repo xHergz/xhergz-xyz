eaf30351e168a43e8c24aa782b15ea5ae8562c16
Justin <xhergz@gmail.com>
2024-08-12 22:08:07 -0400

    Upgraded all packages.

--- src/utils/date.utils.ts
+++ src/utils/date.utils.ts
@@ -1,17 +1,18 @@
+import { Duration } from "date-fns";
 import { isNil } from "lodash";
 
 export const minSecDuration = (duration: Duration): string => {
-    const minutes = isNil(duration.minutes)
-        ? "00"
-        : duration.minutes.toString().padStart(2, "0");
-    const seconds = isNil(duration.seconds)
-        ? "00"
-        : duration.seconds.toString().padStart(2, "0");
-    return `${minutes}m ${seconds}s`;
+  const minutes = isNil(duration.minutes)
+    ? "00"
+    : duration.minutes.toString().padStart(2, "0");
+  const seconds = isNil(duration.seconds)
+    ? "00"
+    : duration.seconds.toString().padStart(2, "0");
+  return `${minutes}m ${seconds}s`;
 };
 
 export const secondsToMinSec = (totalSeconds: number): string => {
-    const minutes = Math.floor(totalSeconds / 60);
-    const seconds = Math.floor(totalSeconds % 60);
-    return `${minutes}m ${seconds}s`;
+  const minutes = Math.floor(totalSeconds / 60);
+  const seconds = Math.floor(totalSeconds % 60);
+  return `${minutes}m ${seconds}s`;
 };
